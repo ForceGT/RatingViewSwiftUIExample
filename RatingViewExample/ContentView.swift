b260ceae8a73a1cd2875327bfa0ba4f4c5634707
ForceGT <thakkargaurav409@gmail.com>
2021-02-17 00:16:38 +0530

    Add example complete

--- RatingViewExample/ContentView.swift
+++ RatingViewExample/ContentView.swift
@@ -6,16 +6,39 @@
 //
 
 import SwiftUI
+import RatingView
 
 struct ContentView: View {
+    
+    private var progressValues : [Float] = [0.1,0.1,0.1,0.1,0.1]
+    private var userRating = 0
+    private var netRate : Float = 4.0
+    // Make an observable object like so
+    var ratingViewModel : RatingViewModel
+    
+    init(){
+        // Add all your initial parameters
+        ratingViewModel = RatingViewModel(progressValues: progressValues, userRating: userRating, netRate: netRate)
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        // Make sure you have the viewmodel and the callback
+        RatingView(ratingViewModel: ratingViewModel, onRatingTapped: updateValues)
     }
-}
+    
+    func updateValues(rating: Int){
+        ratingViewModel.netRate = getRandomFloat()
+        ratingViewModel.progressValues = [getRandomFloat(), getRandomFloat(),getRandomFloat(), getRandomFloat(),getRandomFloat()]
+    }
+    func getRandomFloat() -> Float{
+        return Float.random(in: 0..<1)
+    }
+    }
+    
+    
+    
+    
+    
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+
+
