9e41d24df57d02ee493db3bf005b3d1af983f2cf
ForceGT <thakkargaurav409@gmail.com>
2021-02-16 19:59:32 +0530

    Initial Commit

---
+++ RatingViewExample/RatingViewExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RatingViewExampleApp.swift
+//  RatingViewExample
+//
+//  Created by Gaurav Thakkar on 16/02/21.
+//
+
+import SwiftUI
+
+@main
+struct RatingViewExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
